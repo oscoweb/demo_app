c7120043d6ac87abc8ec222eac86b72bb7ebcfd2
Oscoweb <bb@oscoweb.com>
2013-04-29 14:15:49 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0370957a0b21eda065eda20ab7a7db68f6923e537190a3339e75f109582cc7300135717040c309bd25313110afd92e66c712ad6e2d56721ae40f68a69d137dbc'
